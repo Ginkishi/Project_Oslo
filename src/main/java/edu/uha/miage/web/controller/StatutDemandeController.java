ffc62e469005243ad36cfce5889e58e0e10c873d
Ginkishi <vicmarvu98@gmail.com>
2020-11-29 11:56:33 +0100

    Ajout du composant StatutDemande

---
+++ src/main/java/edu/uha/miage/web/controller/StatutDemandeController.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uha.miage.web.controller;
+
+import edu.uha.miage.core.entity.Domaine;
+import edu.uha.miage.core.service.DomaineService;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author victo
+ */
+@Controller
+@RequestMapping("/statutDemande")
+public class StatutDemandeController {
+         @Autowired
+    DomaineService statutDemandeService;
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public String findAll(Model model) {
+        model.addAttribute("statutDemandes", statutDemandeService.findAll());
+        return "statutDemande/list";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        Domaine statutDemande = new Domaine();
+        model.addAttribute("statutDemande", statutDemande);
+        return "statutDemande/edit";
+    }
+
+
+    @PostMapping("/create")
+    public String created(@Valid Domaine statutDemande, BindingResult br) {
+
+        if (br.hasErrors()) {
+            return "statutDemande/edit";
+        }
+        statutDemandeService.save(statutDemande);
+        return "redirect:/statutDemande";
+    }
+
+    @GetMapping("/edit")
+    public String edit(@RequestParam(name = "id") Long id, Model model) {
+        model.addAttribute("statutDemande", statutDemandeService.findById(id).get());
+        return "statutDemande/edit";
+    }
+
+    @PostMapping("/edit")
+    public String edited(@Valid Domaine statutDemande, BindingResult br) {
+        if (br.hasErrors()) {
+            return "statutDemande/edit";
+        }
+
+        statutDemandeService.save(statutDemande);
+        return "redirect:/statutDemande";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        statutDemandeService.delete(id);
+        return "redirect:/statutDemande";
+    }
+}
