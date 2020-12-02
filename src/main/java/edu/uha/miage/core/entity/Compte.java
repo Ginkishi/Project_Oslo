9945c14650e8f0a17303d125062b75527b8136e9
Ginkishi <vicmarvu98@gmail.com>
2020-12-02 09:26:34 +0100

    Création de compte et d'une personne via un formulaire

---
+++ src/main/java/edu/uha/miage/core/entity/Compte.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uha.miage.core.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author victo
+ */
+@Entity
+public class Compte implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Size(max = 25)
+    // Nom d'utilisateur
+    private String username;
+
+    @NotNull
+    @Size(min = 8)
+    // Mot de passe
+    private String password;
+    
+    @ManyToOne
+    @JoinColumn(name = "personne_id")
+    Personne personne;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    Role role;
+
+    public Compte(String username, String password, Personne personne, Role role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.personne = personne;
+        this.role = role;
+    }
+
+    public Compte() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Personne getPersonne() {
+        return personne;
+    }
+
+    public void setPersonne(Personne personne) {
+        this.personne = personne;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    
+}
